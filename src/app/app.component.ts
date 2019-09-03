533c1a3a04bfbdfc32b655127970f79f937d13c3
Todd Lovas <toddlovas@quickenloans.com>
2019-09-03 13:03:51 -0400

    updated imports on app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Injectable } from '@angular/core';
-import { ClockTableComponent } from './clock-table/clock-table.component';
-import { ClockInOutComponent } from './clock-in-out/clock-in-out.component';
-import { ServeTimeService } from '../serve-time.service';
-
 
 @Component({
   selector: 'app-root',
